fad5839a1646cc1bb4a7d83e9011bf71d5be4f0a
Renan Henrique <renan.costa@b8one.com>
2020-12-14 03:57:04 -0300

    feat: add slick slider and setup desktop page banners

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,39 @@
 import "./../sass/main.scss";
+import $ from "jquery";
+import slick from "slick-carousel";
+/**
+ * Home page class
+ */
+class Home {
+  /**
+   * Construction of home page class
+   */
+  constructor() {
+    this.pageLoad();
+  }
 
+  /**
+   * Init page carousel sliders
+   */
+  initSliders() {
+    const banners = $(".js-main-banners");
+
+    banners.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+    });
+  }
+
+  /**
+   * On page load method
+   */
+  pageLoad() {
+    document.addEventListener("DOMContentLoaded", () => {
+      this.initSliders();
+    });
+  }
+}
+
+new Home();
